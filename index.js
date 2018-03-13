801872b55a6f447dd62d74c1f0a441cde9e25cf4
Richard Trapanese <richardtrapanese@gmail.com>
2018-03-13 02:03:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,41 @@
 const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"];
+//learn  test = to test the code
+//learn submit = to submit code
+
+function destructivelyAppendKitten(name){
+  //appends a kitten to the end of the kittens array
+  return kittens.push(name);
+}
+function destructivelyPrependKitten(name){
+  //prepends a kitten to the beginning of the kittens array
+  return kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten(){
+  //removes the last kitten from the kittens array
+  return kittens.pop();
+}
+function destructivelyRemoveFirstKitten(){
+  //removes the First kitten from the kittens array
+  return kittens.shift();
+}
+function appendKitten(name){
+  //appends add to end, a kitten to the kittens array and returns a new array, leaving the kittens array unchanged
+  const newAddArray = kittens.push(name); //[name, ...kittens];
+  return newAddArray;
+}
+function prependKitten(name){
+  //prepends adds to beginning, a kitten to the kittens array and returns a new array, leaving the kittens array unchanged
+  const newAddArray = kittens.unshift(name);
+  return newAddArray;
+}
+function removeLastKitten(){
+  //removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged
+  const newAddArray = kittens.pop();
+  return newAddArray;
+}
+function removeFirstKitten(){
+  //removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged
+  const newAddArray = kittens.shift();
+  return newAddArray;
+}
