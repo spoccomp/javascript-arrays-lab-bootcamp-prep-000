a16ab8b55bbd141fdb88cfa11cae430b51662d86
Richard Trapanese <richardtrapanese@gmail.com>
2018-03-13 02:22:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function removeLastKitten(){
   //const newAddArray = kittens.pop();
   //return newAddArray;
   //return kittens.pop();
-  const newAddArray = kittens.pop();
+  const newAddArray = kittens.slice(1);
   return newAddArray;
 }
 function removeFirstKitten(){
